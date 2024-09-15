5b64587e027e4670e395b7031127da36fca3bf96
yuniskim <yuniskimani2015@gmail.com>
2024-09-14 23:36:47 -0700

    wk1python

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name= input("enter your name")
+age= input("enter your age")
+location= ("enter location")
+
+print(name)
+print(age)
+print(location)
+
+print(f"my name is {name}, i am {age} years and live in {location}")
